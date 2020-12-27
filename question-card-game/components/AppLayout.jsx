116662fa64ebcc82462f2f63deb2ffd06a4000b0
HoSoo Lee <hosoo.lee@arithnea.de>
2020-12-27 21:54:19 +0100

    *eslint install

--- question-card-game/components/AppLayout.jsx
+++ question-card-game/components/AppLayout.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
 
 const AppLayout = () => {
-  return <div>This is AppLayout</div>;
+  return (
+    <div>
+      This is AppLayout
+      <div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/game">
+          <a>game</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 AppLayout.propTypes = {};
---
+++ question-card-game/pages/game.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function game() {
+  return <div>This is game Page</div>;
+}
+
+export default game;
