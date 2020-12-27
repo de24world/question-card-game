18cab4e6fff651b2a6248bee1f59a3910b2997e0
HoSoo Lee <hosoo.lee@arithnea.de>
2020-12-27 21:31:15 +0100

    proptypes install & set pages

---
+++ question-card-game/components/AppLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AppLayout = () => {
+  return <div>This is AppLayout</div>;
+};
+
+AppLayout.propTypes = {};
+
+export default AppLayout;
