ab32ad990bf92bb3da4d8af44fcba97c74317f92
HoSoo Lee <hosoo.lee@arithnea.de>
2020-12-31 02:11:50 +0100

    지역 설정

--- love-game/pages/index.js
+++ love-game/pages/index.js
@@ -1,11 +1,19 @@
 import MenuHeader from "../src/components/MenuHeader";
 import styles from "../styles/Home.module.css";
 
+import { useRouter } from "next/router";
+
 const Home = () => {
+  const router = useRouter();
+  const { locale, locales, defaultLocale } = router;
+
   return (
     <>
       <MenuHeader />
       <div>this is index page</div>
+      <p>Current locale: {locale}</p>
+      <p>Default locale: {defaultLocale}</p>
+      <p>Configured locales: {JSON.stringify(locales)}</p>
     </>
   );
 };
